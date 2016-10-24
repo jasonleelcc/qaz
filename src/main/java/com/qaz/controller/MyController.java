46e5531a6f0576f4d2e48b62c4ddc30ebd009e33
jasonleelcc <jasonleelcc@hotmail.com>
2016-10-24 16:16:58 +0800

    small change 7

--- src/main/java/com/qaz/controller/MyController.java
+++ src/main/java/com/qaz/controller/MyController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MyController {
     @RequestMapping("/")
-    String home(){return "AP is Running";}
+    String home(){return "AP is Running  ";}
 
     @RequestMapping("/test")
     String test(){return "test  ";}
